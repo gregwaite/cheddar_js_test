13f735f5d373ca8afd2943e8a47b206d593f90bb
Greg Waite <waite68@gmail.com>
2019-10-16 21:10:17 -0700

    Pass tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import test from './test.json'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import test from "./test.json";
 
 function App() {
-  console.log(test)
+  console.log(test);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Use the json coming from "test" (imported at the top) to create a simple reporter displaying passing tests, failing tests, and any related information using simple, reusable components. Contact julia@cheddar.com if you have any questions.
+          Use the json coming from "test" (imported at the top) to create a
+          simple reporter displaying passing tests, failing tests, and any
+          related information using simple, reusable components. Contact
+          julia@cheddar.com if you have any questions.
         </p>
       </header>
     </div>
